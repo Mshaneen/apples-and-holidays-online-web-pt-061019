d35a18b87d97bfa22ede3fdb64f6d3463e49a940
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-07-13 17:31:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,35 +18,13 @@ def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
 
-second_supply_for_fourth_of_july(holiday_supplies)
-
-
 def add_supply_to_winter_holidays(holiday_hash, item)
  holiday_hash[:winter].each do |holiday, decorations|
    decorations << item
   end
 end
 
-# My alternative
-def add_supply_to_winter_holidays(holiday_hash, item)
- holiday_hash[:winter].each do |holiday, decorations|
-   decorations.push(item)
-  end
-end
-
-
 add_supply_to_winter_holidays(holiday_supplies, "Baloons")
-
-# This will not word because the sub-hashes are not somple array-string values
-#def add_supply_to_winter_holidays(holiday_hash, supply)
-#  holiday_hash.each do |season, holiday|
-#    holiday.each do |holiday, supplies|
-#      if holiday == :winter
-#        supplies.push{supply}
-#      end
-#    end
-#  end
-#end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
@@ -57,20 +35,7 @@ end
 
 add_supply_to_memorial_day(holiday_supplies, "baloons")
 
-
-#def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-#  holiday_hash[season][holiday_name] = supply_array
-#  holiday_hash
-#
-#end
-
-
-# //////////////////////////////////////////////////////////////////////////
-# the problem here is that I cannot create another season...the seasons are
-# locked in for some bizzare reason.
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
